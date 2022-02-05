77b4dc7dbaf800a42a6a2b71f7a25b5b168fdcbe
Divya Bhutani <divyabhutani33@gmail.com>
2022-02-04 20:40:57 -0500

    Added project settings component

--- jira-frontend/src/app/project/project-settings/project-settings.component.ts
+++ jira-frontend/src/app/project/project-settings/project-settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 import { ApiInterfaceService } from 'src/app/api-interface.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectSettings, project_data } from '../project-list/project-list.component';
 
 @Component({
   selector: 'app-project-settings',
@@ -8,31 +10,52 @@ import { ApiInterfaceService } from 'src/app/api-interface.service';
   styleUrls: ['./project-settings.component.scss']
 })
 export class ProjectSettingsComponent implements OnInit {
-  myForm = new FormGroup({
-    projectName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    projectDescription: new FormControl('', [Validators.required]),
-  });
+  // myForm = new FormGroup({
+  //   projectName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  //   projectDescription: new FormControl('', [Validators.required]),
+  // });
 
-  constructor(private apiService:ApiInterfaceService) { }
+  project_settings: ProjectSettings | undefined;
+  projectDetailsForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private apiService:ApiInterfaceService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const projectIdFromRoute = Number(routeParams.get('projectId'));
+    this.project_settings = project_data.find((project:any) => project.id === projectIdFromRoute)
+    console.log(this.project_settings)
+
+    // set the form values
+    this.setFormValues();
   }
 
-  get f(){
-    return this.myForm.controls;
+  // get f(){
+  //   return this.myForm.controls;
+  // }
+  setFormValues(): void{
+    this.projectDetailsForm = this.fb.group({
+      id:this.project_settings?.id,
+      name:this.project_settings?.name,
+      key:this.project_settings?.key,
+      lead:this.project_settings?.lead,
+      type:this.project_settings?.type
+    });
   }
-
-  submit(){
-    let body = {
-      name:this.myForm.get('projectName')?.value,
-      description:this.myForm.get('projectDescription')?.value
-    } 
-    this.apiService.updateProjectDetails(body)
-    .subscribe(res => {
-          console.log(res);
+  // submit(){
+  //   let body = {
+  //     name:this.myForm.get('projectName')?.value,
+  //     description:this.myForm.get('projectDescription')?.value
+  //   } 
+  //   this.apiService.updateProjectDetails(body)
+  //   .subscribe(res => {
+  //         console.log(res);
           
-    })
+  //   })
     
+  // }
+  onSubmit(): void{
+    console.log(this.projectDetailsForm);
   }
 
 }
