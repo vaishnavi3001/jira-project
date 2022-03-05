74ef82dc1fb879dbea446b52680a7f6760882754
Divya Bhutani <divyabhutani33@gmail.com>
2022-03-04 21:42:05 -0500

    Cypress: test cases added

---
+++ jira-frontend/cypress/integration/file1/testcase.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+    it('Visit homepage', () => {
+        cy.visit('http://localhost:4200/')
+        cy.contains('Create')
+        cy.contains('Your Work')
+        cy.contains('People')
+        cy.contains('Filters')
+        cy.contains('Projects')
+        cy.contains('App')
+
+      })
+      it('View issues', () => {
+        cy.visit('http://localhost:4200/home/issues/1')
+        cy.contains('Sprint')
+       
+
+      })
+      it('View Sprints', () => {
+        cy.visit('http://localhost:4200/home/sprint/list')
+        cy.contains('Sprint1')
+        cy.contains('Create delete button')
+        cy.contains('Create Add button')
+        cy.contains('Sprint2')
+        cy.contains('Create add API')
+        cy.contains('Create Delete API')
+        cy.contains('Write test cases')
+       
+
+      })
+      
+      
+        
+        
+    
+  })
