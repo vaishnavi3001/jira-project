e01803a1fbd4bc92f002a62bd15a1733e945b12e
pypalkar23 <mandar.palkar94@gmail.com>
2022-03-05 21:24:31 -0500

    chore: Refactoring database initialization

--- jira-backend/main.go
+++ jira-backend/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	"jira-backend/dbutils"
 	"jira-backend/handlers"
-	"jira-backend/models"
 	"jira-backend/utils"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -17,25 +16,15 @@ var db *gorm.DB
 var err error
 var config *viper.Viper
 
-func setDbconnInContext(c *gin.Context) {
-	c.Set("db", db)
-	c.Next()
-}
-
 func setConfigInContext(c *gin.Context) {
 	c.Set("config", config)
 	c.Next()
 }
 
 func main() {
-	db, err = gorm.Open(sqlite.Open("jira.db"), &gorm.Config{})
-	if err != nil {
-		fmt.Println("Cannot Connect to the Database Exiting", err)
-		os.Exit(1)
-	}
 
 	// Migrate the schema
-	db.AutoMigrate(&models.User{}, &models.Project{}, &models.Sprint{}, &models.Issue{}, &models.Permission{}, &models.Issue{}, &models.UserRole{})
+	dbutils.InitializeConn()
 	//,&models.Project{}, &models.Issue{}, &models.Sprint{},  &models.UserRole{}, &models.Role{}, &models.Permission{}
 
 	//viper config
@@ -47,7 +36,6 @@ func main() {
 
 	r := gin.Default()
 	r.Use(setConfigInContext)
-	r.Use(setDbconnInContext)
 
 	/*user := models.User{Name: "Mandar Palkar", Username: "pypalkar23", EmailId: "mandar.palkar23@gmail.com"}
 	db.Create(&user)
@@ -56,33 +44,31 @@ func main() {
 
 	project := r.Group("/project")
 	{
-		project.POST("/create", handlers.CreateProject)
 		project.GET("/list", handlers.ListProjects)
 		project.GET("/info", handlers.GetProjectInfo)
 		project.POST("/delete", handlers.DeleteProject)
+		project.POST("/create", handlers.CreateProject)
 	}
 
 	sprint := r.Group("/sprint")
 	{
-		sprint.POST("/create", handlers.CreateSprint)
 		sprint.GET("/list", handlers.ListSprints)
 		sprint.GET("/info", handlers.GetSprintInfo)
+		//sprint.POST("/create", handlers.CreateSprint)
 		sprint.POST("/delete", handlers.DeleteSprint)
 
-		issue := r.Group("/issues")
-		{
-			issue.POST("/create", handlers.CreateIssue)
-			issue.GET("/list", handlers.ListIssue)
-			issue.GET("/info", handlers.GetIssueInfo)
-			issue.POST("/delete", handlers.DeleteIssue)
-			issue.POST("/update", handlers.UpdateIssue)
-		}
-
-		ip_address := fmt.Sprintf("%s:%d", config.GetString("server.ip_address"), config.GetInt("server.port"))
-		r.Run(ip_address)
-
-		/*project1 := models.Project{Name: "Jira-Clone", OwnerId: 1, CreatedAt: time.Now()}
-		db.Create(&project1)*/
-
 	}
+
+	issue := r.Group("/issues")
+	{
+		issue.GET("/list", handlers.ListIssue)
+		issue.GET("/info", handlers.GetIssueInfo)
+		issue.POST("/create", handlers.CreateIssue)
+		issue.POST("/delete", handlers.DeleteIssue)
+		issue.POST("/update", handlers.UpdateIssue)
+	}
+
+	ip_address := fmt.Sprintf("%s:%d", config.GetString("server.ip_address"), config.GetInt("server.port"))
+	r.Run(ip_address)
+
 }
