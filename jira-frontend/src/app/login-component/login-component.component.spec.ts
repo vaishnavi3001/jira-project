614a55d6b340542c7be496c40acaa14a7c2b5cb9
Divya Bhutani <divyabhutani33@gmail.com>
2022-04-01 20:09:50 -0400

    unit testcases added

--- jira-frontend/src/app/login-component/login-component.component.spec.ts
+++ jira-frontend/src/app/login-component/login-component.component.spec.ts
@@ -1,25 +1,74 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
+import {
+  ComponentFixture,
+  //fakeAsync,
+  TestBed,
+  //tick,
+ // waitForAsync,
+} from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+//import { Login-Component } from './login-component.component';
 import { LoginComponentComponent } from './login-component.component';
+//import { LoginService } from './login.service';
+import { ApiInterfaceService } from 'src/app/api-interface.service';
 
-describe('LoginComponentComponent', () => {
-  let component: LoginComponentComponent;
+class Page {
+  get submitButton() {
+    return this.fixture.nativeElement.querySelector('button');
+  }
+  get usernameInput() {
+    return this.fixture.debugElement.nativeElement.querySelector('#username');
+  }
+  get passwordInput() {
+    return this.fixture.debugElement.nativeElement.querySelector('#pwd');
+  }
+
+  get errorMsg() {
+    return this.fixture.debugElement.query(By.css('.error')).nativeElement;
+  }
+
+  constructor(private fixture: ComponentFixture<LoginComponentComponent>) {}
+
+  public updateValue(input: HTMLInputElement, value: string) {
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+  }
+}
+describe('Login Component', () => {
+  let loginComponent: LoginComponentComponent;
   let fixture: ComponentFixture<LoginComponentComponent>;
+  let debugEl: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponentComponent ]
-    })
-    .compileComponents();
-  });
-
+  let loginService: ApiInterfaceService ;
+  let loginServiceSpy: { login: jasmine.Spy };
+  let routerSpy: { navigateByUrl: jasmine.Spy };
+  let router: Router;
+  let page: Page;
   beforeEach(() => {
+    loginServiceSpy = jasmine.createSpyObj(ApiInterfaceService , ['login']);
+    routerSpy = jasmine.createSpyObj(Router, ['navigateByUrl']);
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [LoginComponentComponent],
+      providers: [
+        { provide: ApiInterfaceService , useValue: loginServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+    });
     fixture = TestBed.createComponent(LoginComponentComponent);
-    component = fixture.componentInstance;
+    loginComponent = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    loginService = TestBed.inject(ApiInterfaceService );
+    router = TestBed.inject(Router);
+    page = new Page(fixture);
     fixture.detectChanges();
+
+    
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
