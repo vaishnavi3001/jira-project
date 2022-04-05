39a9e55dff85f14c05c77d3428f629963fdcf005
Divya Bhutani <divyabhutani33@gmail.com>
2022-04-05 19:39:22 -0400

    Added test cases for login

--- jira-frontend/src/app/login-component/login-component.component.spec.ts
+++ jira-frontend/src/app/login-component/login-component.component.spec.ts
@@ -25,4 +25,13 @@ describe('LoginComponentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should have',()=>{
+    const title = fixture.debugElement.nativeElement.querySelector('.modal-title');
+    expect(title.innerHTML).not.toBeNull();
+    const footer = fixture.debugElement.nativeElement.querySelector('.modal-footer');
+    expect(footer.innerHTML).not.toBeNull();
+    
+  })
+
+
 });
