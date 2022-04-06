74f9e78aff0771ee41db8673116f1ef912e88e6e
Divya Bhutani <divyabhutani33@gmail.com>
2022-04-05 23:51:32 -0400

    added testcases for register

--- jira-frontend/src/app/register/register.component.spec.ts
+++ jira-frontend/src/app/register/register.component.spec.ts
@@ -25,4 +25,12 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have',()=>{
+    const title = fixture.debugElement.nativeElement.querySelector('.modal-title');
+    expect(title.innerHTML).not.toBeNull();
+    const footer = fixture.debugElement.nativeElement.querySelector('.modal-footer');
+    expect(footer.innerHTML).not.toBeNull();
+    
+  })
 });
