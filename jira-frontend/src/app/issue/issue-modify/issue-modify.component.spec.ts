ec4d015f115958edf83b0bf10656a32cf45d2f73
Divya Bhutani <divyabhutani33@gmail.com>
2022-04-12 12:56:07 -0400

    updated testcases for issue-modify

--- jira-frontend/src/app/issue/issue-modify/issue-modify.component.spec.ts
+++ jira-frontend/src/app/issue/issue-modify/issue-modify.component.spec.ts
@@ -26,6 +26,8 @@ describe('IssueModifyComponent', () => {
   it('should have',()=>{
     const header= fixture.debugElement.nativeElement.querySelector('.card-header');
     expect(header.innerHTML).not.toBeNull();
+    const random_text = fixture.debugElement.nativeElement.querySelector('.row');
+    expect(random_text.innerHTML).not.toBeNull();
     const body = fixture.debugElement.nativeElement.querySelector('.card-body');
     expect(body.innerHTML).not.toBeNull();
     
