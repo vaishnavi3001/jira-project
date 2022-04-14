c65792897a8b36bd6c9378cd7689102b47516a03
amhaske <amhaske32@gmail.com>
2022-04-13 21:43:48 -0400

    chore:added cdk droplist to drag the issues from one list to another.

--- jira-frontend/src/app/sprint/sprint-detail/sprint-detail.component.ts
+++ jira-frontend/src/app/sprint/sprint-detail/sprint-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-sprint-detail',
@@ -11,5 +12,21 @@ export class SprintDetailComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  Upcoming = [];
+  InProgress = [];
+  Review = [];
+  Done = [];
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }  
 
 }
