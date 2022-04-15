5c59590f25a85dda36f21ab3a23cc717b60f7b5b
pypalkar23 <mandar.palkar94@gmail.com>
2022-04-15 18:49:18 -0400

    chore: changing port for the frontend

--- jira-frontend/build/launch.go
+++ jira-frontend/build/launch.go
@@ -1,22 +1,21 @@
 package main
+
 import (
-	"net/http"
 	"embed"
+	"fmt"
 	"io/fs"
 	"log"
-	"fmt"
+	"net/http"
 )
-
 
 //go:embed assets/*
 var static embed.FS
 
-
 func main() {
-	host :=  "0.0.0.0"
-	port := "8080"
+	host := "0.0.0.0"
+	port := "5000"
 	contentStatic, _ := fs.Sub(static, "assets/jira-frontend")
 	http.Handle("/", http.FileServer(http.FS(contentStatic)))
-	log.Fatal(http.ListenAndServe(host+":"+port,nil))
-	fmt.Printf("Server is running at %s:%s",host, port)
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
+	fmt.Printf("Server is running at %s:%s", host, port)
 }
