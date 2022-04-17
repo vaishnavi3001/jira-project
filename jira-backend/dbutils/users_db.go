c0030bf9cef26361eb46a1f3322bc852688c1eb2
vaishnavi3001 <dongre.vaishnavi@gmail.com>
2022-04-16 23:12:57 -0400

    feature: change password

--- jira-backend/dbutils/users_db.go
+++ jira-backend/dbutils/users_db.go
@@ -69,3 +69,25 @@ func UpdateProfileUser(user_id uint, data sk.UserProfile) gin.H {
 	resp := sk.UserProfile{Username: data.Username, Email: data.Email, FirstName: data.FirstName, LastName: data.LastName}
 	return ut.GetSuccessResponse(ct.USER_PROFILE_UPDATE_SUCCESS, resp)
 }
+
+func ChangePassword(user_id uint, data sk.ChangePasswordReq) gin.H {
+	oldPassword := data.OldPassword
+	
+	newPassword := data.NewPassword
+	fmt.Println(oldPassword)
+	fmt.Println("###")
+	fmt.Println(newPassword)
+	fmt.Println("@@@")
+	var count int64
+	var user md.User
+	
+	DB.Where("user_id = ? AND password = ?", user_id, oldPassword).Find(&user).Count(&count)
+	
+	if count == 0{
+		return ut.GetErrorResponse(ct.INVALID_CREDENTIALS)
+	}
+
+	DB.Model(&md.User{}).Where("user_id = ?", user_id).Updates(md.User{Password: newPassword})
+	resp := ""
+	return ut.GetSuccessResponse(ct.PASSWORD_CHANGE_SUCCESSFUL, resp)
+}
