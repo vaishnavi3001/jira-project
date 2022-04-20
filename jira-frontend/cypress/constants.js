c22c850bb44a06a9e8db8d9437ae5d5327e987ff
amhaske <amhaske32@gmail.com>
2022-04-20 19:50:59 -0400

    chore:temp commit

---
+++ jira-frontend/cypress/constants.js
@@ -0,0 +1,9 @@
+export const constant_values = {
+    REGISTER: "/register",
+    LOGIN: "/login",
+    PROJECT_LIST: "/projects",
+    // EDIT_POST: "/edit/1",
+    // SINGLE_POST: "/post/1",
+    // GET_POSTS: "/myposts",
+    // USER_PROFILE: "/userprofile"
+};
---
+++ jira-frontend/cypress/integration/2.login test/login.js
@@ -0,0 +1,17 @@
+import { constant_values } from "../../constants";
+// import 'cypress-localstorage-commands'
+
+
+describe("Checks Login", () => {
+
+    it("should test the login page", () => {
+      cy.visit(`${constant_values.LOGIN}`);
+      cy.get("[id=email]").type("pypalkar23");
+      cy.get("[id=password]").type("mandar");
+  
+      cy.get("[id=login-button]").click();
+      // cy.saveLocalStorage();
+      localStorage.setItem('access_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NTA1MDA3OTQsImlhdCI6MTY1MDQ5NzE5NH0.c14W1sS-vjg9lYWY03GPKsnjkGzPPJrrb_6KTgkXlL0')
+    });
+  
+  });
